b74005e58592f400b5ece0284d6b1d1fe710c6fb
Phyllis Wong <wong.phyllism@gmail.com>
2018-01-25 13:04:19 -0800

    add method stubs for testing, contains, add, remove func

--- source/set.py
+++ source/set.py
@@ -1,40 +1,57 @@
 #!python
 
-from hashtable import Hashtable
 from pprint import pprint
 
 
-class Set(Object):
+class Set(object):
 
     def __init__(self, elements=None):
         '''initialize a new empty set structure, and add each element if a sequence is given.'''
-        self.buckets = [LinkedList() for i in range(elements)]
         self.size = 0  # Number of entries
 
+        if elements is None:
+            # Hashtable initializes with 8 buckets if elements=None
+            self.data = HashTable()
+        else:
+            self.data(HashTable(len(elements)))
+
+    def __str__(self):
+        '''Return a formatted string representation of this hash table.
+        {!r} chooses repr() to format the value passed in.'''
+        items = ['{!r}'.format(item) for item in self.items()]
+        return '{' + ', '.join(items) + '}'
+
+    def __repr__(self):
+        '''Return a string representation of this hash table.'''
+        return 'HashTable({!r})'.format(self.items())
+
     def contains(self, element):
-        '''return a boolean indicating whether element is in this set'''
-        pass
+        '''Return a boolean indicating whether element is in this set.'''
+        '''Use the method from hashtable class to test if contains.'''
+        contain = self.data.contains(element)
+        return contain
 
     def add(self, element):
-        '''add element to this set, if not present already'''
-        pass
+        '''Add element to this set, if not present already.'''
+        # Use hashtable set func, takes a key, value pair
+        self.data.set(element, None)
 
     def remove(self, element):
-        '''remove element from this set, if present, or else raise KeyError'''
-        pass
+        '''Remove element from this set, if present, or else raise KeyError.'''
+        self.data.delete(element)
 
     def union(self, other_set):
-        '''return a new set that is the union of this set and other_set'''
+        '''Return a new set that is the union of this set and other_set.'''
         pass
 
     def intersection(self, other_set):
-        '''return a new set that is the intersection of this set and other_set'''
+        '''Return a new set that is the intersection of this set and other_set.'''
         pass
 
     def difference(self, other_set):
-        '''return a new set that is the difference of this set and other_set'''
+        '''Return a new set that is the difference of this set and other_set.'''
         pass
 
     def is_subset(self, other_set):
-        '''return a boolean indicating whether other_set is a subset of this set'''
+        '''Return a boolean indicating whether other_set is a subset of this set.'''
         pass
---
+++ source/set_test.py
@@ -0,0 +1,38 @@
+#!python
+
+from hashtable import HashTable
+import unittest
+# Python 2 and 3 compatibility: unittest module renamed this assertion method
+if not hasattr(unittest.TestCase, 'assertCountEqual'):
+    unittest.TestCase.assertCountEqual = unittest.TestCase.assertItemsEqual
+
+
+class SetTest(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_add(self):
+        pass
+
+    def test_remove(self):
+        pass
+
+    def test_contains(self):
+        pass
+
+    def test_union(self):
+        pass
+
+    def test_intersection(self):
+        pass
+
+    def test_difference(self):
+        pass
+
+    def test_subset(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
